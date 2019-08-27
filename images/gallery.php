cda4e315d3a49a898042c566f8404ad505770627
Oluwaseun Joseph  Amodeni <44782724+oluwaseunAyodele@users.noreply.github.com>
2019-08-27 12:35:55 +0100

    Add files via upload

---
+++ images/gallery.php
@@ -0,0 +1,601 @@
+<?php
+
+//include("headerstuff.php");
+?>
+	
+
+<?php $pagetitle="-Gallery -"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?php echo $pagetitle; ?></title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Course Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/main_stylesxx.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/teachers_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/teachers_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<?php
+	include("insert_upper_links.php");
+	
+	?>
+	
+	
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background_container prlx_parent">
+			<div class="home_background prlx" style="background-image:url(images/teachers_background.jpg)"></div>
+		</div>
+		<div class="home_content">
+			<h2 style="color:white"><b>Gallery</b></h2>
+		</div>
+	</div>
+	
+	
+	<!-- GALLERY -->
+	
+	
+	
+	<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font: 14px/1 'Open Sans', sans-serif;
+  color: #555;
+  background: #fff;
+}
+
+.gallery {
+  width: 640px;
+  margin: 0 auto;
+  padding: 5px;
+  background: #fff;
+  box-shadow: 0 1px 2px rgba(0,0,0,.3);
+}
+
+.gallery > div {
+  position: relative;
+  float: left;
+  padding: 5px;
+}
+
+.gallery > div > img {
+  display: block;
+  width: 200px;
+  transition: .1s transform;
+  transform: translateZ(0); /* hack */
+}
+
+.gallery > div:hover {
+  z-index: 1;
+}
+
+.gallery > div:hover > img {
+  transform: scale(1.7,1.7);
+  transition: .3s transform;
+}
+
+.cf:before, .cf:after {
+  display: table;
+  content: "";
+  line-height: 0;
+}
+
+.cf:after {
+  clear: both;
+}
+
+h1 {
+  margin: 40px 0;
+  font-size: 30px;
+  font-weight: 300;
+  text-align: center;
+}
+</style>
+
+
+ 
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font: 14px/1 'Open Sans', sans-serif;
+  color: #555;
+  background: #fff;
+}
+
+.gallery {
+  width: 640px;
+  margin: 0 auto;
+  padding: 5px;
+  background: #fff;
+  box-shadow: 0 1px 2px rgba(0,0,0,.3);
+}
+
+.gallery > div {
+  position: relative;
+  float: left;
+  padding: 5px;
+}
+
+.gallery > div > img {
+  display: block;
+  width: 200px;
+  transition: .1s transform;
+  transform: translateZ(0); /* hack */
+}
+
+.gallery > div:hover {
+  z-index: 1;
+}
+
+.gallery > div:hover > img {
+  transform: scale(1.7,1.7);
+  transition: .3s transform;
+}
+
+.cf:before, .cf:after {
+  display: table;
+  content: "";
+  line-height: 0;
+}
+
+.cf:after {
+  clear: both;
+}
+
+h1 {
+  margin: 40px 0;
+  font-size: 30px;
+  font-weight: 300;
+  text-align: center;
+}
+</style>
+
+
+<h1></h1>
+
+<div class="gallery cf">
+  <div>
+    <img src="images/gallery/8.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/7.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/2.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/3.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/4.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/5jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/6.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/7.png" />
+  </div>
+   <div>
+    <img src="images/gallery/8.jpg" />
+  </div>
+   <div>
+    <img src="images/gallery/9.jpg" />
+  </div>
+  
+  <div>
+    <img src="images/gallery/10.jpg" />
+  </div>
+  <div>
+   <img src="images/gallery/11.jpg" />
+  </div>
+  <div>
+   <img src="images/gallery/12.jpg" />
+  </div>
+  <div>
+    <img src="images/gallery/13.jpg" />
+  </div>
+  <div>
+   <img src="images/gallery/14.jpg" />
+  </div>
+</div>
+
+
+	<!-- GALLERY -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<center>
+
+	<!-- Become -->
+
+	<div class="become">
+		<div class="container">
+			<div class="row row-eq-height">
+
+				<div class="col-lg-6 order-2 order-lg-1">
+					<div class="become_title">
+						<h1>Funpee Kitchen and Events Services</h1>
+					</div>
+					<p class="become_text">		
+				-----
+					
+					</p>
+					
+					
+					
+						<p class="become_text">
+		
+					</p>
+					
+					
+					<div class="become_button text-center trans_200">
+						<a href="contact.php">Contact Us</a>
+					</div>
+				</div>
+
+				<div class="col-lg-6 order-1 order-lg-2">
+					<div class="become_image">
+						<!--<img src="images/logo_2.png" alt="Funpee Kitchen & Events"  >-->
+					</div>
+					<hr>
+					<img src="images/logo_2.png" alt="Funpee Kitchen & Events"  width=300 height=250>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	
+	
+	
+	
+
+
+	<!-- Become -->
+
+	<div class="become">
+		<div class="container">
+			<div class="row row-eq-height">
+
+				<div class="col-lg-6 order-2 order-lg-1">
+					<div class="become_title">
+						<h1>Funpee Products & Services</h1>
+					</div>
+					<p class="become_text">		
+				-----
+					
+					</p>
+					
+					
+					
+						<p class="become_text">
+		
+					</p>
+					
+					
+					<div class="become_button text-center trans_200">
+						<a href="contact.php">Contact Us</a>
+					</div>
+				</div>
+
+				<div class="col-lg-6 order-1 order-lg-2">
+					<div class="become_image">
+						<!--<img src="images/logo_2.png" alt="Funpee Kitchen & Events"  >-->
+					</div>
+					<hr>
+					<img src="images/logo.png" alt="Funpee Kitchen & Events"  width=300 height=250>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	</center>
+	
+	<?php
+	/**
+	
+
+	<!-- Teachers -->
+
+	<div class="teachers page_section">
+		<div class="container">
+			<div class="row">
+				
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_1.jpg" alt="https://unsplash.com/@michaeldam">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Fruity Juicy </a></div>
+							<div class="card-text">Sorrel Juice(Zobo)</div>
+							<div class="teacher_social">
+								
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_2.jpg" alt="https://unsplash.com/@jcpeacock">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Fruity Juicy </a></div>
+							<div class="card-text">Chapman</div>
+							<div class="teacher_social">
+								
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_3.jpg" alt="https://unsplash.com/photos/n8jeOSxCRfQ">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Fruity Juicy </a></div>
+							<div class="card-text">Smoothies</div>
+							<div class="teacher_social">
+								
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_4.jpg" alt="https://unsplash.com/@seteales">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Fruity Juicy  </a></div>
+							<div class="card-text">Tigernut Drink</div>
+							<div class="teacher_social">
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_5.jpg" alt="https://unsplash.com/@jaredsluyter">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Funpees Delite </a></div>
+							<div class="card-text">Popcorn
+</div>
+							<div class="teacher_social">
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Teacher -->
+				<div class="col-lg-4 teacher">
+					<div class="card">
+						<div class="card_img">
+							<div class="card_plus trans_200 text-center"><a href="#">+</a></div>
+							<img class="card-img-top trans_200" src="images/teacher_6.jpg" alt="https://unsplash.com/@mehdizadeh">
+						</div>
+						<div class="card-body text-center">
+							<div class="card-title"><a href="#">Funpees Delite </a></div>
+							<div class="card-text">Spicy Chin chin
+</div>
+							<div class="teacher_social">
+								<!--
+								<ul class="menu_social">
+									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								</ul>
+								-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	
+	
+	
+	**/?>
+	<!-- Milestones -->
+
+	<div class="milestones">
+		<div class="milestones_background" style="background-image:url(images/milestones_background.jpg)"></div>
+
+		<div class="container">
+			<div class="row">
+				
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon">
+						<!--
+						<img src="images/milestone_1.svg" alt="https://www.flaticon.com/authors/zlatko-najdenovski">
+						-->
+						</div>
+						<div class="milestone_counter" data-end-value="750">0</div>
+						<div class="milestone_text">Current training cadre</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon">
+						<!--
+						<img src="images/milestone_2.svg" alt="https://www.flaticon.com/authors/zlatko-najdenovski">
+						-->
+						
+						</div>
+						<div class="milestone_counter" data-end-value="120">0</div>
+						<div class="milestone_text">Satisfied Clientele</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon">
+						<!--
+						<img src="images/milestone_3.svg" alt="https://www.flaticon.com/authors/zlatko-najdenovski">
+						-->
+						</div>
+						<div class="milestone_counter" data-end-value="39">0</div>
+						<div class="milestone_text">Milestone Portfolio</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon">
+						<!--
+						<img src="images/milestone_4.svg" alt="https://www.flaticon.com/authors/zlatko-najdenovski">
+						-->
+						</div>
+						<div class="milestone_counter" data-end-value="3500" data-sign-before="+">0</div>
+						<div class="milestone_text">Youth Empowerment</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	
+	<!-- Footer -->
+
+		<?php
+
+include("insert_footerstuff.php");
+?>
+
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/scrollTo/jquery.scrollTo.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/teachers_custom.js"></script>
+
+</body>
+</html>
